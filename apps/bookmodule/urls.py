c87cb538aa390bdbd274a0b24d89a171e63d04cb
Lubna-mg <lubna.mg@hotmail.com>
2025-03-03 21:40:08 +0300

    Initial commit for lab4 project

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,9 +1,10 @@
 ﻿from django.urls import path
-from . import views
+from .views import index, list_books, aboutus
+
+app_name = 'books'  # تأكد من وجود `app_name`
 
 urlpatterns = [
-    path('', views.index),  
-    path('<int:bookId>/', views.viewbook),
+    path('', index, name='index'),  # تعديل الاسم ليكون "index" بدون "books."
+    path('list/', list_books, name='list_books'),
+    path('about/', aboutus, name='aboutus'),
 ]
-
-
